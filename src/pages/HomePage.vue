57a39a29cc5ac7e9f3aa1af9b648ced4fc07d55a
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-18 09:29:54 -0600

    home page styled

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,35 +3,20 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid bg-success">
+    <div class="row">
+      <div class="col-12 text-center">
+        <img
+          src="https://user-images.githubusercontent.com/102513373/208978485-ee3a19b6-ebd8-4745-b0a2-7e509552146c.png"
+          alt="Gregslist logo">
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+img {
+  height: var(--main-height);
+  object-fit: cover;
 }
 </style>
